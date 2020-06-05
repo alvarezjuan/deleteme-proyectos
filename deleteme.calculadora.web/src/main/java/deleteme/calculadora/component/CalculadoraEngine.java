4c1c8f66d40a7665466a96bce1d32ab93dcd20bd
Juan Alvarez <carlosa@gmlsoftware.com>
2020-06-05 14:52:34 -0500

    Implementacion inicial del motor de calculo - inclusion del engine

---
+++ deleteme.calculadora.web/src/main/java/deleteme/calculadora/component/CalculadoraEngine.java
@@ -0,0 +1,9 @@
+package deleteme.calculadora.component;
+
+public class CalculadoraEngine {
+    
+    public String Calcular(String primerOperando, String operador, String segundoOperando, String calcular) {
+        return "Adicionar el codigo aqui de calculo";
+    }
+
+}
